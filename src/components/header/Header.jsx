9ce64122ac87043438bc893f09284bb972ef9c3a
Henok Kirubel <henokhm2@gmail.com>
2023-03-20 00:50:05 +0300

    make header responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,24 +5,35 @@ import CurrencyMenu from "./CurrencyMenu";
 
 function Header() {
   const { windowWidth } = useWindowSize();
-  const BREAKPOINT = 820;
+  const BREAKPOINT = 920;
+  const showLogoText =
+    windowWidth > 1150 || (windowWidth <= BREAKPOINT && windowWidth > 700);
+
   return (
     <header className="bg-safflower-red w-full flex flex-col justify-center gap-4 p-4">
-      <div className="max-w-7xl mx-auto h-32 flex items-center gap-12 px-4 md:px-8 py-2">
+      <div className="max-w-[1440px] w-full mx-auto h-32 flex items-center justify-center gap-12 px-4 md:px-8 py-2">
         <img
-          className="h-full"
+          className="h-20 sm:h-full"
           src="/Logo.png"
           alt="anime accessory shop logo"
         />
-        <div className="flex flex-col gap-0 items-center text-white font-bold text-lg min-w-max">
-          <span>Anime Acsessory Shop</span>
-          <span>“Otaku’s world”</span>
-        </div>
+        {showLogoText && (
+          <div className="flex flex-col gap-0 items-center text-white font-bold text-lg min-w-max">
+            <span>Anime Acsessory Shop</span>
+            <span>“Otaku’s world”</span>
+          </div>
+        )}
+
         {windowWidth > BREAKPOINT && <Searchbar />}
         <LanguageMenu />
         <CurrencyMenu />
       </div>
-      {windowWidth <= BREAKPOINT && <Searchbar />}
+      {windowWidth <= BREAKPOINT && (
+        <div className="mx-8">
+          <Searchbar />
+        </div>
+      )}
+      <div className="absolute top-0 right-0 bg-white p-1">{windowWidth}</div>
     </header>
   );
 }
--- src/components/header/Searchbar.jsx
+++ src/components/header/Searchbar.jsx
@@ -8,7 +8,7 @@ function Searchbar() {
   };
 
   return (
-    <div className="rounded-3xl bg-birch-white grow h-10 flex">
+    <div className="grow rounded-3xl bg-birch-white h-10 flex">
       <input
         className="h-10 bg-transparent text-gray-600 rounded-3xl grow px-5 outline-none focus:ring-0"
         type="text"
@@ -17,7 +17,7 @@ function Searchbar() {
         onChange={handleQueryChange}
       />
 
-      <button className=" w-10 h-10 bg-bonfire rounded-full flex justify-center items-center hover:rotate-12 hover:scale-105 duration-100">
+      <button className="w-10 h-10 bg-bonfire rounded-full flex justify-center items-center hover:rotate-12 hover:scale-105 duration-100">
         <img src="/svg/search-icon.svg" alt="search icon" />
       </button>
     </div>
