82b08069976a9005c1f9f44b292eeb365ca8b59f
Henok Kirubel <henokhm2@gmail.com>
2023-03-19 23:28:23 +0300

    add searchbar component

--- src/components/header/Searchbar.jsx
+++ src/components/header/Searchbar.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
 function Searchbar() {
-  return <div>Searchbar</div>;
+  const [query, setQuery] = useState("");
+
+  const handleQueryChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  return (
+    <div className="rounded-3xl bg-birch-white grow h-10 flex">
+      <input
+        className="h-10 bg-transparent text-gray-600 rounded-3xl grow px-5 outline-none focus:ring-0"
+        type="text"
+        placeholder="Search..."
+        value={query}
+        onChange={handleQueryChange}
+      />
+
+      <button className=" w-10 h-10 bg-bonfire rounded-full flex justify-center items-center hover:rotate-12 hover:scale-105 duration-100">
+        <img src="/svg/search-icon.svg" alt="search icon" />
+      </button>
+    </div>
+  );
 }
 
 export default Searchbar;
