c4577cf7ebe4c8ea2135bc2c87c1adc7d77cfa78
Henok Kirubel <henokhm2@gmail.com>
2023-03-19 23:40:45 +0300

    make header responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,28 @@
 import Searchbar from "./Searchbar";
+import useWindowSize from "../../hooks/useWindowSize";
 
 function Header() {
+  const { windowWidth } = useWindowSize();
+  const BREAKPOINT = 820;
   return (
-    <header className="bg-safflower-red w-full h-32 ">
-      <div className="max-w-6xl mx-auto h-32 flex items-center gap-8 px-4 md:px-8 py-6">
+    <header className="bg-safflower-red w-full flex flex-col justify-center gap-4 p-4">
+      <div className="max-w-7xl mx-auto h-32 flex items-center gap-12 px-4 md:px-8 py-2">
         <img
           className="h-full"
           src="/Logo.png"
           alt="anime accessory shop logo"
         />
-        <div className="flex flex-col gap-0 items-center text-white font-bold text-lg">
+        <div className="flex flex-col gap-0 items-center text-white font-bold text-lg min-w-max">
           <span>Anime Acsessory Shop</span>
           <span>“Otaku’s world”</span>
         </div>
-        <Searchbar />
+        {windowWidth > BREAKPOINT && <Searchbar />}
         <div className="text-white flex gap-8">
           <span>Language Dropdown</span>
           <span>Currency Dropdown</span>
         </div>
       </div>
+      {windowWidth <= BREAKPOINT && <Searchbar />}
     </header>
   );
 }
