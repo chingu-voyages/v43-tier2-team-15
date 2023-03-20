85701a9fc9179b336fcef401fce63fd7d4b8b670
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-03-20 20:08:51 +0200

    added the responsive design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,46 +5,50 @@ import telegram__icon from "../../public/images/telegram__icon.png";
 
 export default function Footer() {
   return (
-    <div className="footer h-80 bg-[#d63447] not-italic flex justify-around items-center">
-      <div className="footer__img h-40 w-40">
-        <img src={footer__logo} alt="“Otaku’s world” logo" />
+    <div className="footer h-full bg-[#d63447] py-2 not-italic md:h-80 md:flex md:justify-around md:items-center">
+      <div className="footer__img h-30 w-30 md:h-40 md:w-40 ">
+        <img
+          src={footer__logo}
+          alt="“Otaku’s world” logo"
+          className="mx-auto md:m-0"
+        />
       </div>
-      <div className="footer__contactUs">
-        <h4 className="font-bold text-2xl leading-7 text-left text-black">
+      <div className="footer__contactUs text-center mt-3  pt-1 md:mt-0 md:text-left">
+        <h4 className="text-black font-bold text-xl text-center md:text-2xl md:leading-7 md:text-left ">
           Contact us
         </h4>
         <div className="footer__contactUs-icons flex justify-center items-center">
           <img
             src={instagram__icon}
             alt="“Otaku’s world” instagram page"
-            className="my-7 mr-7"
+            className="my-2 mx-2 md:my-7 md:mr-7"
           />
 
           <img
             src={facebook__icon}
             alt="“Otaku’s world” facebook page"
-            className="my-7 mr-7"
+            className="my-2 mx-2 md:my-7 md:mr-7"
           />
 
           <img
             src={telegram__icon}
             alt="“Otaku’s world” telegram page"
-            className="my-7 mr-7"
+            className="my-2 mx-2 md:my-7 md:mr-7"
           />
         </div>
         <a
-          className="footer__contactUs-phone font-bold text-lg leading-5 text-center text-black no-underline"
+          className="footer__contactUs-phone font-bold text-md text-black no-underline md:leading-5 md:text-lg  "
           href="tel:+3805454544615"
         >
           +3805454544615
         </a>
       </div>
-      <div className="footer__about mt-10">
-        <h4 className="font-bold text-2xl leading-7 text-left text-black">
+      <div className="footer__about mt-3 md:mt-10">
+        <h4 className="font-bold text-xl text-black text-center md:text-2xl md:leading-7 md:text-left ">
           About
         </h4>
-        <ul className="mt-5 list-none">
-          <li className="mb-3">
+        <ul className="md:mt-5 md:text-left list-none text-center">
+          <li className="mb-1 md:mb-3">
             <a
               href="/"
               className="font-normal text-lg leading-5 text-center no-underline text-black"
@@ -52,7 +56,7 @@ export default function Footer() {
               Company
             </a>
           </li>
-          <li className="mb-3">
+          <li className="mb-1 md:mb-3">
             <a
               href="/"
               className="font-normal text-lg leading-5 text-center no-underline text-black"
@@ -60,7 +64,7 @@ export default function Footer() {
               FAQ
             </a>
           </li>
-          <li className="mb-3">
+          <li className="mb-1 md:mb-3">
             <a
               href="/"
               className="font-normal text-lg leading-5 text-center no-underline text-black"
@@ -68,7 +72,7 @@ export default function Footer() {
               Delivery
             </a>
           </li>
-          <li className="mb-3">
+          <li className="mb-1 md:mb-3">
             <a
               href="/"
               className="font-normal text-lg leading-5 text-center no-underline text-black"
@@ -78,11 +82,11 @@ export default function Footer() {
           </li>
         </ul>
       </div>
-      <div className="footer__workSchedule">
-        <h4 className="font-bold text-2xl leading-7 text-center text-black">
+      <div className="footer__workSchedule mt-3 md:mt-0">
+        <h4 className="font-bold text-xl md:text-2xl leading-7 text-center text-black">
           Work schedule
         </h4>
-        <div className="footer__workSchedule-text mt-12 font-normal text-lg leading-5 text-center text-black">
+        <div className="footer__workSchedule-text  font-normal text-lg leading-5 text-center text-black mt-2 md:mt-12">
           <p>Schedule for Call center</p>
           <p>Tuesday - Sunday from 11 to 17</p>
           <p>Physical store - from 11 to 19</p>
