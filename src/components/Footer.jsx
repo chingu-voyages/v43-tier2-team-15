348741ba68b2249ee1af4dfda1e1aec551002005
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-03-20 18:12:02 +0200

    added images to the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,22 @@
+import footer__logo from "../../public/images/footer__img.png";
+import instagram__icon from "../../public/images/instagram__icon.png";
+import facebook__icon from "../../public/images/facebook__icon.png";
+import telegram__icon from "../../public/images/telegram__icon.png";
+
 export default function Footer() {
   return (
     <div className="footer">
       <div className="footer__img">
-        <img src="/" alt="“Otaku’s world” logo" />
+        <img src={footer__logo} alt="“Otaku’s world” logo" />
       </div>
       <div className="footer__contactUs">
         <h4>Contact us</h4>
         <div className="footer__contactUs-icons">
-          <img src="/" alt="“Otaku’s world” instagram page" />
+          <img src={instagram__icon} alt="“Otaku’s world” instagram page" />
 
-          <img src="/" alt="“Otaku’s world” facebook page" />
+          <img src={facebook__icon} alt="“Otaku’s world” facebook page" />
 
-          <img src="/" alt="“Otaku’s world” telegram page" />
+          <img src={telegram__icon} alt="“Otaku’s world” telegram page" />
         </div>
         <a className="footer__contactUs-phone" href="tel:+3805454544615">
           +3805454544615
