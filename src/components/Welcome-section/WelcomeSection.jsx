334c2eb38b1b2cc54a96627f8880a17953800974
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-03-28 21:42:42 +0300

    added the background image

--- src/components/Welcome-section/WelcomeSection.jsx
+++ src/components/Welcome-section/WelcomeSection.jsx
@@ -1,6 +1,13 @@
 export default function WelcomeSection() {
   return (
     <div className="welcome">
+      <div className="welcome__img w-full h-[724px] relative">
+        <img
+          src="./././public/images/welcome__img.png"
+          alt="Welcome to “Otaku’s world”"
+          className="w-full h-full z-0 object-cover absolute top-0 left-0"
+        />
+      </div>
       <h1 className="welcome__title">
         Welcome to: <br /> <span>“Otaku’s world”</span>
       </h1>
