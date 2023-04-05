245679108815967379a5b7db9683940de150a06f
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-04-05 15:20:39 +0300

    added the page's html template

---
+++ src/components/World-delivery/WorldDelivery.jsx
@@ -0,0 +1,31 @@
+export default function WorldDelivery() {
+  return (
+    <div >
+      <div >
+        <img
+          src="../../public/images/world-delivery.png"
+          alt="World delivery image"
+        />
+      </div>
+      <div >
+        <h3 >
+          World Delivery
+        </h3>
+        <div >
+          <p>
+            Our company provides world delivery so every each of our clients can
+            get what they want to buy even in the farthest places from our
+            online shop.
+          </p>
+          <p >
+            The estimated price for delivery depends on a country that you are
+            living in so to know more about how we proceed with the world
+            delivery please read the infrormation down below.
+          </p>
+        </div>
+        <div >
+        </div>
+      </div>
+    </div>
+  );
+}
