b998a419483c55c946790fc805c8d1c0cccd7ffc
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-04-05 22:33:27 +0300

    added the world delivery component to the App file

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from "./components/UI/NavBar";
 import WelcomeSection from "./components/Welcome-section/WelcomeSection";
 import OffLineShop from "./components/offLine-shop/OffLine-shop";
 import Filters from "./components/Layout/Filters";
+import WorldDelivery from "./components/World-delivery/WorldDelivery";
 
 function App() {
   return (
@@ -72,6 +73,7 @@ function App() {
       </main>
       <Filters />
       <Card id={6} />
+      <WorldDelivery/>
       <OffLineShop />
       <Footer />
     </div>
