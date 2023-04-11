b4986ac590ae1eb487fb5cd7434f8cbee0f29613
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-04-11 21:15:55 +0300

    added margins and the background color

--- src/components/pages/ProductItemPage.jsx
+++ src/components/pages/ProductItemPage.jsx
@@ -1,28 +1,28 @@
 export default function ProductItem() {
   return (
-    <div className="productItem">
+    <div className="productItem bg-birch-white">
       <div className="productItem__main flex">
         <div className="productItem__main-img w-1/2"></div>
         <div className="productItem__main-info w-1/2">
           <h2 className="not-italic font-bold text-5xl leading-10 text-black">
             Taito Hatsune Miku Figure Costumes China Dress Ver.
           </h2>
-          <p className="not-italic font-normal text-sm leading-4 text-left text-black">
+          <p className="not-italic font-normal text-sm leading-4 text-left text-black mt-2">
             item code: 858808
           </p>
-          <div className="flex">
+          <div className="flex mt-3">
             <div className="productItem__main-infoCircle rounded-full w-6 h-6 bg-green-700 mr-3"></div>
             <span className="not-italic font-normal text-2xl leading-7 flex items-center text-center text-black">
               In stock
             </span>
           </div>
-          <ul className="not-italic font-normal text-2xl leading-7 list-none text-black">
+          <ul className="not-italic font-normal text-2xl leading-7 list-none text-black mt-20">
             <li>Hatsune Miku figure from Taito!</li>
             <li>Base Stand included </li>
             <li>Multiple Colors</li>
             <li>Package Weight: 2.0 pounds</li>
           </ul>
-          <div className="productItem__main-infoPrice">
+          <div className="productItem__main-infoPrice mt-20">
             <h4 className="not-italic font-bold text-4xl leading-10 text-black">
               40 euros
             </h4>
