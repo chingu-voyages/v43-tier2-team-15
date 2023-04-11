c3589c9d529e5d3b5ff8d728008623889c150b68
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-04-11 20:33:37 +0300

    added the Product Item Page Route

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import App from "../App";
 import ProductList from "./pages/ProductListPage";
+import ProductItem from "./pages/ProductItemPage"
+
 
 export default function RoutesList() {
   return (
     <Routes>
       <Route exact path="/" element={<App />} />
       <Route path="/products" element={<ProductList />} />
+      <Route path="/products" element={<ProductItem/>}/>
     </Routes>
   );
 }
