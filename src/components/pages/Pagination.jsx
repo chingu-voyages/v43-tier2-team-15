84625034e200595fd98cdf5b51cfe98b3fb9d453
Sasha K <nortsandra@gmail.com>
2023-04-17 19:46:57 +0300

    added styling to pagination

--- src/components/pages/Pagination.jsx
+++ src/components/pages/Pagination.jsx
@@ -17,14 +17,18 @@ export default function Pagination({
   };
 
   return (
-    <div className="pagination flex justify-center">
+    <div className="pagination flex flex-nowrap justify-center items-center space-x-1">
       {pageNumbers.map((pageNumber) => (
         <button
           key={pageNumber}
           onClick={() => handleClick(pageNumber)}
-          className={`pagination-btn ${
-            currentPageIndex === pageNumber ? "active" : ""
-          }`}>
+          className={`pagination-btn 
+              ${currentPageIndex === pageNumber ? "active" : ""}
+              px-4 py-2 rounded-md 
+              bg-bonfire hover:bg-orange-700 
+              text-white font-medium 
+              focus:outline-none focus:ring-2 focus:ring-orange-700 focus:ring-opacity-50 
+              sm:text-sm sm:px-3 md:text-base md:px-4`}>
           {pageNumber + 1}
         </button>
       ))}
