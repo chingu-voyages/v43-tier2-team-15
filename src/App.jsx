eb6a90ab6bf7d1e0992bee1c7158ddf7738000fa
Sasha K <nortsandra@gmail.com>
2023-04-18 21:23:47 +0300

    added tailwing classes for better look

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import Header from "./components/header/Header";
 import Footer from "./components/Footer";
-import Card from "./components/card/Card";
 import NavBar from "./components/UI/NavBar";
 import WelcomeSection from "./components/Welcome-section/WelcomeSection";
 import OfflineShop from "./components/offline-shop/OfflineShop";
-import Filters from "./components/Layout/Filters";
 import WorldDelivery from "./components/World-delivery/WorldDelivery";
 import ProductItem from "./components/pages/ProductItemPage";
 
@@ -15,8 +13,6 @@ function App() {
       <NavBar />
       <WelcomeSection />
       <ProductItem />
-      <Filters />
-      <Card id={6} />
       <WorldDelivery />
       <OfflineShop />
       <Footer />
--- src/components/pages/ProductListPage.jsx
+++ src/components/pages/ProductListPage.jsx
@@ -29,95 +29,111 @@ export default function ProductList() {
         alt=""
         src="src/assets/images/banner.png"
       />
-      <div class="filter-sort">
-        <div class="row align-items-center">
-          <div class="col-auto d-none d-lg-block font-roboto font-medium text-xs leading-4 tracking-widest text-left">
-            <label>SORT BY:</label>
+      <div className="grid grid-cols-2">
+        <div className="left-panel mt-[55px]">
+          <div className="breadcrumb ml-[65px] mb-[45px]">
+            <nav class="flex" aria-label="Breadcrumb">
+              <ol class="inline-flex items-center space-x-1 md:space-x-3">
+                <li class="inline-flex items-center">
+                  <a
+                    href="#"
+                    class="inline-flex items-center text-sm font-normal text-gray-800 hover:text-orange-600">
+                    <svg
+                      aria-hidden="true"
+                      class="w-4 h-4 mr-2"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+                    </svg>
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <div class="flex items-center">
+                    <svg
+                      aria-hidden="true"
+                      class="w-6 h-6 text-gray-800"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        fill-rule="evenodd"
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        clip-rule="evenodd"></path>
+                    </svg>
+                    <a
+                      href="#"
+                      class="ml-1 text-sm font-normal text-gray-800 md:ml-2 hover:text-orange-600">
+                      Anime action figures
+                    </a>
+                  </div>
+                </li>
+              </ol>
+            </nav>
           </div>
-          <div class="col">
-            <select class="js-filter-sort input--sort block rounded-md border border-gray-500 bg-transparent px-3 py-2 shadow-sm focus:border-gray-600 focus:outline-none focus:ring-gray-600 sm:text-sm">
-              <option value="" selected class="text-gray-800">
-                Most Rated
-              </option>
-              <option value="-n-stock" class="text-gray-800">
-                In stock
-              </option>
-              <option value="low-to-high" class="text-gray-800">
-                Price: Low → High
-              </option>
-              <option value="high-to-low" class="text-gray-800">
-                Price: High → Low
-              </option>
-              <option value="by-recommended" class="text-gray-800">
-                Recommended
-              </option>
-              <option value="recently-added" class="text-gray-800">
-                Recently Added
-              </option>
-              <option value="low-rating" class="text-gray-800">
-                Lowest Rated
-              </option>
-            </select>
+          <div class="filters-catalogue ml-[65px] flex flex-col gap-[780px]">
+            <div class="catalogue relative">
+              <Catalogue />
+            </div>
+            <div class="filter mb-[80px]">
+              <Filters />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="breadcrumb">
-        <nav class="flex" aria-label="Breadcrumb">
-          <ol class="inline-flex items-center space-x-1 md:space-x-3">
-            <li class="inline-flex items-center">
-              <a
-                href="#"
-                class="inline-flex items-center text-sm font-medium text-gray-400 hover:text-gray-600">
-                <svg
-                  aria-hidden="true"
-                  class="w-4 h-4 mr-2"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-                </svg>
-                Home
-              </a>
-            </li>
-            <li>
-              <div class="flex items-center">
-                <svg
-                  aria-hidden="true"
-                  class="w-6 h-6 text-gray-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fill-rule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clip-rule="evenodd"></path>
-                </svg>
-                <a
-                  href="#"
-                  class="ml-1 text-sm font-medium text-gray-400 md:ml-2 hover:text-gray-600">
-                  Anime action figures
-                </a>
+        <div className="products-list-sort mr-[65px]">
+          <div class="filter-sort px-0 py-10 float-right">
+            <div class="row align-items-center">
+              <div class="col-auto d-none d-lg-block font-roboto font-medium text-xs leading-4 tracking-widest text-left">
+                <label>SORT BY:</label>
               </div>
-            </li>
-          </ol>
-        </nav>
-      </div>
-      <Catalogue />
-      <Filters />
-      <div className="product-list grid grid-cols-3 gap-16 w-[870px]">
-        {currentItems.map((item) => (
-          <Card
-            key={item}
-            id={item}
-            currentPage={currentPage}
-            itemsPerPage={itemsPerPage}
-          />
-        ))}
-        <Pagination
-          itemsPerPage={itemsPerPage}
-          totalItems={productList.length}
-          setCurrentPage={setCurrentPage}
-        />
+              <div class="col">
+                <select class="js-filter-sort input--sort block rounded-md border border-gray-500 bg-transparent px-3 py-2 shadow-sm focus:border-gray-600 focus:outline-none focus:ring-gray-600 sm:text-sm">
+                  <option value="" selected class="text-gray-800">
+                    Most Rated
+                  </option>
+                  <option value="-n-stock" class="text-gray-800">
+                    In stock
+                  </option>
+                  <option value="low-to-high" class="text-gray-800">
+                    Price: Low → High
+                  </option>
+                  <option value="high-to-low" class="text-gray-800">
+                    Price: High → Low
+                  </option>
+                  <option value="by-recommended" class="text-gray-800">
+                    Recommended
+                  </option>
+                  <option value="recently-added" class="text-gray-800">
+                    Recently Added
+                  </option>
+                  <option value="low-rating" class="text-gray-800">
+                    Lowest Rated
+                  </option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="products">
+            <div className="product-list grid grid-cols-3 gap-16 w-[870px]">
+              {currentItems.map((item) => (
+                <Card
+                  key={item}
+                  id={item}
+                  currentPage={currentPage}
+                  itemsPerPage={itemsPerPage}
+                />
+              ))}
+              <div className="pagination">
+                <Pagination
+                  itemsPerPage={itemsPerPage}
+                  totalItems={productList.length}
+                  setCurrentPage={setCurrentPage}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
