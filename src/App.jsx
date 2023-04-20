be8b91b83939e381b8458e4334fd81cf27598560
Sasha K <nortsandra@gmail.com>
2023-04-20 22:00:36 +0300

    imported card

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import WelcomeSection from "./components/Welcome-section/WelcomeSection";
 import OfflineShop from "./components/offline-shop/OfflineShop";
 import WorldDelivery from "./components/World-delivery/WorldDelivery";
 import Popular from "./components/PopularItems";
+import Card from "./components/card/Card";
 import { Link } from "react-router-dom";
 
 function App() {
